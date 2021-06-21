03460e8357534890502ddaf17bd3e3d97ab92a18
Mohammad Haghighat <hosseinhg@hotmail.com>
2021-06-21 11:59:27 +0430

    Update api doc for transaction and invoice APIs

--- api_project.js
+++ api_project.js
@@ -9,8 +9,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-04-17T07:38:01.760Z",
+    "time": "2021-06-21T07:28:49.999Z",
     "url": "https://apidocjs.com",
-    "version": "0.27.1"
+    "version": "0.28.1"
   }
 });
